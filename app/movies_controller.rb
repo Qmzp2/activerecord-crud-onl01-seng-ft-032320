4505d95361db31849d734eebd18c91741c84c376
Kevin Mathias <komathias1@gmail.com>
2020-05-03 04:50:26 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,7 @@ def can_be_created_in_a_block(args = ["Home Alone", 1990] )
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = args[0]
-    m.release_date = args[1]
+    m = args
   end
 end
 
